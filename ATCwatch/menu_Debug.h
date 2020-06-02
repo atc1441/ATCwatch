ad051815142b76c64bb4883116d1fd7dd6d869d7
atc1441 <1102694+atc1441@users.noreply.github.com>
2020-06-02 13:15:05 +0200

    Added uptime display in debug menu

--- ATCwatch/menu_Debug.h
+++ ATCwatch/menu_Debug.h
@@ -25,17 +25,29 @@ class DebugScreen : public TheScreen
     {
       displayRect(0, 0, 240, 240, 0x0000);
       displayPrintln(0, 0, "Debug:", 0xFF00, 0x0000, 2);
-      displayImage(120 - (72 / 2), 240 - 72, 72, 72, symbolDebug);
+      displayPrintln(0, 20, "Uptime:", 0xFFFF, 0x0000, 2);
       displayPrintln(0, 120 - 16, "Reset: " + (String)NRF_POWER->RESETREAS, 0xFFFF, 0x0000, 2);
-      displayPrintln(0, 120, "Wakeup:", 0xFFFF, 0x0000, 2);
+      displayPrintln(0, 120, "Wakeup: ", 0xFFFF, 0x0000, 2);
+      displayImage(120 - (72 / 2), 240 - 72, 72, 72, symbolDebug);
     }
 
     virtual void main()
     {
-      displayPrintln(0, 20, (String)millis() + "      ", 0xFFFF, 0x0000, 2);
-      displayPrintln(0, 20 + 16, (String)(millis() / 1000) + "      ", 0xFFFF, 0x0000, 2);
-      displayPrintln(0, 20 + 16 + 16, (String)(millis() / 1000 / 60) + "      ", 0xFFFF, 0x0000, 2);
-      displayPrintln(0, 20 + 16 + 16 + 16, (String)(millis() / 1000 / 60 / 60) + "      ", 0xFFFF, 0x0000, 2);
+      long days = 0;
+      long hours = 0;
+      long mins = 0;
+      long secs = 0;
+      secs = millis() / 1000;
+      mins = secs / 60;
+      hours = mins / 60;
+      days = hours / 24;
+      secs = secs - (mins * 60);
+      mins = mins - (hours * 60);
+      hours = hours - (days * 24);
+
+
+      displayPrintln(0, 20 + 16, (String)millis() + "      ", 0xFFFF, 0x0000, 2);
+      displayPrintln(0, 20 + 16 + 16, String(days) + " " + (String)hours + ":" + (String)mins + ":" + (String)secs + "     ", 0xFFFF, 0x0000, 2);
       displayPrintln((9 * 5 * 2), 120, (String)wakeup_reason[get_wakeup_reason()], 0xFFFF, 0x0000, 2);
     }
 
