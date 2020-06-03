398752662a191e12b762f3d274cdbaeab41f2b5d
atc1441 <1102694+atc1441@users.noreply.github.com>
2020-06-03 13:47:16 +0200

    Update sleep.cpp

--- ATCwatch/sleep.cpp
+++ ATCwatch/sleep.cpp
@@ -82,8 +82,8 @@ void check_sleep_times() {
   bool temp_sleep = false;
   if (millis() - lastaction > get_sleep_time_menu())
   temp_sleep = true;
-  if(get_wakeup_reason()== WAKEUP_ACCL && !get_is_looing_at())
-  temp_sleep = true;
+ // if(get_wakeup_reason()== WAKEUP_ACCL && !get_is_looing_at())
+ // temp_sleep = true;
   if(temp_sleep)
     sleep_down();
 }
