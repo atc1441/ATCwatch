fbf143e70caa46f010b35d99ea816ceb2dbc384e
atc1441 <1102694+atc1441@users.noreply.github.com>
2020-06-14 00:25:38 +0200

    Delete menuAppsBase.h

--- ATCwatch/menuAppsBase.h
+++
@@ -1,7 +0,0 @@
-
-#pragma once
-#include "Arduino.h"
-
-void displayIconAndText(uint32_t x,uint32_t y,uint32_t w,uint32_t h,String text,const uint16_t *buff);
-void menuAppsBase(uint32_t page);
-int check_app_click(uint32_t x,uint32_t y);
