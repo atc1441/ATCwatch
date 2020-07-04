0a30f907fd3c392bbbd8b6deafcb96d45a35d8dd
atc1441 <1102694+atc1441@users.noreply.github.com>
2020-07-04 11:15:25 +0200

    Added SWD Debugmode Status in DebugScreen

--- ATCwatch/menu_Debug.h
+++ ATCwatch/menu_Debug.h
@@ -32,16 +32,16 @@ class DebugScreen : public Screen
       lv_obj_align(label_uptime, NULL, LV_ALIGN_IN_TOP_LEFT, 10, 45);
 
       label_reset = lv_label_create(lv_scr_act(), NULL);
-      lv_label_set_text(label_reset, "Reset:");
-      lv_obj_align(label_reset, NULL, LV_ALIGN_IN_TOP_LEFT, 10, 65);
       lv_label_set_text_fmt(label_reset, "Reset: %i", NRF_POWER->RESETREAS);
+      lv_obj_align(label_reset, NULL, LV_ALIGN_IN_TOP_LEFT, 10, 65);
 
       label_wakeup = lv_label_create(lv_scr_act(), NULL);
-      lv_label_set_text(label_wakeup, "Wakeup:");
+      lv_label_set_text_fmt(label_wakeup, "Wakeup: %s", wakeup_reason[get_wakeup_reason()]);
       lv_obj_align(label_wakeup, NULL, LV_ALIGN_IN_TOP_LEFT, 10, 85);
-      char wakeup_string[14];
-      sprintf(wakeup_string, "Wakeup: %s", wakeup_reason[get_wakeup_reason()]);
-      lv_label_set_text_fmt(label_wakeup, wakeup_string);
+
+      label_debug = lv_label_create(lv_scr_act(), NULL);
+      lv_label_set_text_fmt(label_debug, "Debugmode: %s", (DWT->CYCCNT ? "Disabled":"Enabled"));
+      lv_obj_align(label_debug, NULL, LV_ALIGN_IN_TOP_LEFT, 10, 105);
 
       lv_obj_t * img1 = lv_img_create(lv_scr_act(), NULL);
       lv_img_set_src(img1, &IsymbolDebug);
@@ -75,7 +75,7 @@ class DebugScreen : public Screen
     }
     
   private:
-    lv_obj_t *label, *label_millis, *label_uptime, *label_reset, *label_wakeup;
+    lv_obj_t *label, *label_millis, *label_uptime, *label_reset, *label_wakeup, *label_debug;
     char * wakeup_reason[10] = {"Unset", "Push", "Connect", "Disconnect", "Charged", "Charge", "Button", "Touch", "Accl", "AcclINT"};
 
 };
